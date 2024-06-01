eeb1bdc119b873bcf353f94f38ba27759045d0e2
Vokturz <victornavarrokp47@gmail.com>
2024-06-01 00:14:10 -0400

    include git support

---
+++ src/gitutils.ts
@@ -0,0 +1,74 @@
+const axios = require('axios');
+import fs from 'node:fs/promises';
+const AdmZip = require('adm-zip');
+import path from 'path';
+
+type gitServiceTypes = 'github' | 'gitlab' | 'bitbucket'
+
+export async function downloadAndExtractRepo(gitService: string, user: string, repo: string, branch: string, token: string) {
+    let url, commitUrl, headers
+    switch (gitService){
+        case 'github':
+            url = `https://api.github.com/repos/${user}/${repo}/zipball/${branch}`
+            commitUrl = `https://api.github.com/repos/${user}/${repo}/commits?sha=${branch}&per_page=1`
+            headers = {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${token}`,
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+            break
+        case 'gitlab':
+            url = `https://gitlab.com/api/v4/projects/${user}%2F${repo}/repository/archive.zip?sha=${branch}`
+            commitUrl = `https://gitlab.com/api/v4/projects/${user}%2F${repo}/repository/commits/${branch}`
+            headers = {
+                'Authorization': `Bearer ${token}`
+            }
+            break
+        case 'bitbucket':
+            url = `https://api.bitbucket.org/2.0/repositories/${user}/${repo}/get?at=${branch}`
+            commitUrl = `https://api.bitbucket.org/2.0/repositories/${user}/${repo}/commits/${branch}`
+            headers = {
+                'Authorization': `Bearer ${token}`
+            }
+            break
+    }
+    try {
+        const commitResponse = await axios({
+            method: 'GET',
+            url: commitUrl,
+            headers
+        })
+        const commitSha = commitResponse.data[0].sha
+        console.log(commitSha)
+        try {
+            const response = await axios({
+                method: 'GET',
+                url,
+                responseType: 'arraybuffer',
+                headers
+            })
+
+            const extractPath = `/tmp/${commitSha}_${user}_${repo}_${branch}`
+            // Save zip
+            const zipPath = `${extractPath}.zip`
+            await fs.writeFile(zipPath, response.data)
+
+            // Extract zip
+            const zip = new AdmZip(zipPath)
+            const zipEntries = zip.getEntries()
+            const mainFolderPath = zipEntries.find((entry: any) => entry.isDirectory && entry.entryName.endsWith('/') &&  (entry.entryName.match(/\//g) || []).length == 1).entryName;
+            zip.extractAllTo(extractPath, true)
+            
+            // Delete zip
+            await fs.unlink(zipPath)
+            return path.join(extractPath, mainFolderPath)
+        } catch (error) {
+            console.log(error)
+            throw error
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,10 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
-import {
-  Codebase,
-  // Node
-} from './model/codebase'
-// import { CallsCapturer } from './model/calls'
-// import node from 'tree-sitter-typescript'
-// import fs from 'node:fs/promises';
-
+import { Codebase} from './model/codebase'
+import { tursoClient } from './db/client'
+import { downloadAndExtractRepo } from './gitutils'
+import path from 'path'
 // const 
 const app = new Hono()
 app.use('/v1/*', cors())
@@ -18,21 +14,23 @@ app.use('/v1/*', cors())
 const repoRequestValidator = zValidator(
   'json',
   z.object({
-    user: z.string().optional(),
+    gitService: z.string().optional().default('github'),
+    user: z.string(),
     repo: z.string(),
-    branch: z.string().optional(),
-    token: z.string().optional(),
+    branch: z.string(),
+    token: z.string(),
     update: z.boolean().optional()
   })
 )
 
 app.get('/', async (c) => {
   console.time('codebase')
-  const codebase = new Codebase('../codebase-index-ts') // 
+  const codebasePath = path.join(__dirname, '../../api-vicuna-deno') 
+  const codebase = new Codebase(codebasePath) // 
   const fileNodesMap = await codebase.parseFolder()
-  codebase.getCalls(fileNodesMap, false)
+  codebase.getCalls(fileNodesMap, true)
   console.timeEnd('codebase')
-  const codebaseSimplified = codebase.simplify().filter(c => ['file'].includes(c.type))
+  const codebaseSimplified = codebase.simplify()//.filter(c => ['file'].includes(c.type))
 
   // console.log(codebaseSimplified)
   return c.text(JSON.stringify(codebaseSimplified, null, 2))
@@ -42,19 +40,93 @@ app.get('/', async (c) => {
 
 app.post('/v1/repo', repoRequestValidator, async (c) => {
   const request = c.req.valid('json')
-  const { user, repo, branch, token, update } = request
+  const { gitService, user, repo, branch, token } = request
   // console.log(request)
   console.time(repo)
-  const codebase = new Codebase(`../${repo}`) // 
+  const codebasePath = await downloadAndExtractRepo(gitService, user, repo, branch, token)
+//   tursoClient.execute({
+//     sql: `INSERT INTO git_repositories (git_service, user, repo, branch, token, update) VALUES ( ?, ?, ?, ?, ?, ?)`,
+//     args: [gitService, user, repo, branch, token, update]}
+// )
+  if (!codebasePath) return c.json({ message: 'Failed to download repo' }, 400)
+  const codebase = new Codebase(codebasePath) // 
   const fileNodesMap = await codebase.parseFolder()
   codebase.getCalls(fileNodesMap, false)
   const nodes = codebase.simplify()
   const links = codebase.getLinks()
   console.timeEnd(repo)
-  return c.json({ codebase: {},  graph: { nodes, links } })
+  return c.json({ graph: { nodes, links } })
 
 })
 
+
+app.get('v1/test', async (c) => {
+  const codebasePath = await downloadAndExtractRepo('github', 'JudiniLabs', 'judini-python', 'main', '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')
+  console.log(codebasePath)
+  return c.text('codebase path: ' + codebasePath)
+})
+
+// /**
+//  * CREATE TURSO DATABASE
+//  * 
+//  */
+// import { createClient } from "@libsql/client";
+// export const turso = createClient({
+//   url: process.env.TURSO_DATABASE_URL || '',
+//   authToken: process.env.TURSO_AUTH_TOKEN || '',
+// });
+
+// (async () => {
+//   console.log('Updating DB...')
+//   await turso.execute("PRAGMA foreign_keys = ON")
+
+//   await turso.execute(`
+//   CREATE TABLE IF NOT EXISTS git_repositories (
+//     id TEXT PRIMARY KEY DEFAULT (uuid()),
+//     git_service TEXT CHECK(git_service IN ('github', 'gitlab', 'bitbucket')) NOT NULL,
+//     user TEXT NOT NULL,
+//     repo TEXT NOT NULL,
+//     branch TEXT NOT NULL,
+//     commit_id TEXT,
+//     status TEXT CHECK(status IN ('pending', 'success', 'failure')) DEFAULT 'pending'
+// )
+// `)
+
+
+// await turso.execute(`
+// CREATE TABLE IF NOT EXISTS nodes (
+//   nid TEXT PRIMARY KEY DEFAULT (uuid()),
+//   id TEXT NOT NULL,
+//   git_repositories_id TEXT NOT NULL,
+//   commit_id TEXT,
+//   type TEXT,
+//   name TEXT,
+//   label TEXT,
+//   language TEXT,
+//   exportable BOOLEAN,
+//   totalTokens INTEGER,
+//   documentation TEXT,
+//   code TEXT,
+//   import_statements JSON DEFAULT('[]'),
+//   parent TEXT,
+//   children JSON DEFAULT('[]'),
+//   calls JSON DEFAULT('[]'),
+//   inDegree INTEGER,
+//   outDegree INTEGER,
+//   FOREIGN KEY (git_repositories_id) REFERENCES git_repositories(id)
+// )`)
+
+// await turso.execute(`
+// CREATE TABLE IF NOT EXISTS links (
+//   lid TEXT PRIMARY KEY DEFAULT (uuid()),
+//   source TEXT NOT NULL,
+//   target TEXT NOT NULL,
+//   label TEXT,
+//   FOREIGN KEY (source) REFERENCES nodes(nid),
+//   FOREIGN KEY (target) REFERENCES nodes(nid)
+// )
+// `)
+// })()
 
 
 const port = 8001
