df8d9e7d0214a3f1b74f6d8f9965c1a184dbcae7
Vokturz <victornavarrokp47@gmail.com>
2024-07-10 15:15:10 -0400

    revert index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-// import { serve } from '@hono/node-server'
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
 import { createGraph } from './routes/create_graph'
 import { createGraphTest } from './routes/create_graph-test'
 import { graphs } from './routes/graphs'
-import { handle } from 'hono/aws-lambda'
 
 // const
 const app = new Hono()
@@ -14,14 +13,17 @@ app.use('*', prettyJSON())
 app.use('/v1/*', cors())
 
 app.get('/', async (c) => {
-  return c.json({ message: 'Hello, World!' }, { status: 200 })
+  return c.json({ message: 'Hello, World!' }, {status: 200})
 })
 
 app.route('/v1/repo', createGraph)
 app.route('/v1/graphs', graphs)
 app.route('v1/repo-test', createGraphTest)
 
-const port = Number(process.env.PORT ?? 8001)
+const port = Number(process.env.PORT ?? 8001) 
 console.log(`Server is running on port ${port}`)
 
-export const handler = handle(app)
+serve({
+  fetch: app.fetch,
+  port
+})
